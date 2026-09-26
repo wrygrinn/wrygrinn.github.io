5286c443232489df5da38b1ff64e4eabba58d55e
wrygrinn <wrygrinn@proton.me>
2026-09-26 00:51:05 -0500

    Update email1.js

--- js/email1.js
+++ js/email1.js
@@ -4,3 +4,16 @@
               publicKey: "CIHi6aJpe9m6qfZMH",
             });
         })();
+
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(() => {
+                        console.log('Message Sent!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
