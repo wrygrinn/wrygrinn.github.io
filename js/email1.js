263378554f6fc5b79495fc95eb0dbaa3bc45105e
wrygrinn <wrygrinn@proton.me>
2026-09-26 00:33:34 -0500

    Update email1.js

--- js/email1.js
+++ js/email1.js
@@ -1,3 +1,6 @@
-emailjs.init({
-  publicKey: 'CIHi6aJpe9m6qfZMH',
-});
+      (function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init({
+              publicKey: "CIHi6aJpe9m6qfZMH",
+            });
+        })();
